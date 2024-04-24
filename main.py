197be9f6087064e24f162184475f53fc28005af3
l-don <louis.donath@gmail.com>
2024-04-24 20:42:21 +0200

    updateBuyOrder works

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ X, Y = getAlbionPos()
 print("Albion is at", X, Y)
 time.sleep(0.1)
 
-create_buy_order(item_name="Tasche des Adepten",quantity=10,minimum_difference=1)
+update_buy_order(item_name="Tasche des Adepten",quantity=10,minimum_difference=1,max_pay_amount=2000)
 
 
