7acfefb576a3e88454f6977fbabbede3ecc7c76d
l-don <louis.donath@gmail.com>
2024-04-26 15:03:18 +0200

    add readMe

--- main.py
+++ main.py
@@ -24,10 +24,6 @@ def main():
             collect_items()
             create_sell_order(order.item_name, order.quantity, DIFF)
 
-    # create_sell_order(item_name="Tasche des Adepten", quantity=1, minimum_difference=5)
-    # collect_items()
-    # update_sell_order(item_name="Tasche des Adepten", quantity=3, minimum_difference=5, min_sell_amount=1300)
-
 
 if __name__ == "__main__":
     main()
